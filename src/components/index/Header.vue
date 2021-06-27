914a0962208537525ad1e99fbe0d7cdfda7741d2
Lesken <kblesken@gmail.com>
2021-06-28 01:08:19 +0600

    Add nuxt-link

--- src/components/index/Header.vue
+++ src/components/index/Header.vue
@@ -7,53 +7,50 @@
         <p>Total Deposited Value at HGEN</p>
       </div>
       <div class="launch-app">
-        <a href="/swap" class="btn btn-primary">
-          Launch App
-        </a>
+        <NuxtLink to="/swap" class="btn btn-primary"> Launch App </NuxtLink>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
 
 <style lang="scss" scoped>
-  .header {
-    padding: 1rem 0;
+.header {
+  padding: 1rem 0;
+}
+
+.total-deposited {
+  margin-bottom: 2rem;
+  text-align: center;
+
+  h3 {
+    display: inline-block;
+    font-size: 2.3rem;
+    text-align: center;
+    background: linear-gradient(90deg, #00fff0 0%, #a900ff 100%);
+    color: transparent;
+    -webkit-background-clip: text;
+    background-clip: text;
   }
 
-  .total-deposited {
-    margin-bottom: 2rem;
-    text-align: center;
+  p {
+    color: $text-gray;
+    font-weight: 700;
+    font-size: 1.1rem;
+  }
 
+  @media (max-width: map-get($breakpoints, sm)) {
     h3 {
-      display: inline-block;
-      font-size: 2.3rem;
-      text-align: center;
-      background: linear-gradient(90deg, #00FFF0 0%, #A900FF 100%);
-      color: transparent;
-      -webkit-background-clip: text;
-      background-clip: text;
-    }
-
-    p {
-      color: $text-gray;
-      font-weight: 700;
-      font-size: 1.1rem;
-    }
-
-    @media (max-width: map-get($breakpoints, sm)) {
-      h3 {
-        font-size: 1.8rem;
-      }
+      font-size: 1.8rem;
     }
   }
+}
 
-  .launch-app {
-    margin-bottom: 4rem;
-    text-align: center;
-  }
+.launch-app {
+  margin-bottom: 4rem;
+  text-align: center;
+}
 </style>
