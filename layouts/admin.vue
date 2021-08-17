2edfc33888e5ed67bcc9e521554e10aac4573165
Arman Oralov <arman_oralov@arman.local>
2021-08-18 00:23:04 +0600

    feat: add faq page

---
+++ layouts/admin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-100 h-100min">
+    <div class="w-100 h-100min p-f l-0 t-0 ovh-y-a bg-r" />
+    <div class="w-100 fd-r jc-c pt-10">
+      <div class="container fd-r">
+        <div class="w-85-L w-100-S w-100-XS mcolor rad-fix-10 p-8-M p-3-S p-3-XS">
+          <Nuxt />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ pages/documentation/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="w-100 fd-c ai-c py-6">
+    <div class="container">
+      <div class="d-f ta-l fs-12-L fs-12-M fs-8-S fs-8-XS f-gray-600 fw-400">
+        <div
+          class="mr-9-L mr-8-M mr-4-S mr-4-XS pb-1 c-p" 
+          :class="{'br-mcolor-300': activeTab === 'faq', 'brbs-s': activeTab === 'faq'}"
+          @click="activeTab = 'faq'">
+          FAQ
+        </div>
+        <div
+          class="mr-9-L mr-8-M mr-4-S mr-4-XS pb-1 c-p"
+          :class="{'br-mcolor-300': activeTab === 'guide', 'brbs-s': activeTab === 'guide'}"
+          @click="activeTab = 'guide'">
+          Beginners Guide
+        </div>
+        <div
+          class="mr-9-L mr-8-M mr-4-S mr-4-XS pb-1 c-p" 
+          :class="{'br-mcolor-300': activeTab === 'audit', 'brbs-s': activeTab === 'audit'}"
+          @click="activeTab = 'audit'">
+          Audit
+        </div>
+      </div>
+    </div>
+
+    <div class="container mt-8-L">
+      <Faq v-if="activeTab === 'faq'"/>
+      <BeginnersGuide v-if="activeTab === 'guide'"/>
+      <Audit v-if="activeTab === 'audit'"/>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Faq from '@/components/documentation/Faq'
+import BeginnersGuide from '@/components/documentation/BeginnersGuide'
+import Audit from '@/components/documentation/Audit'
+
+export default {
+  components: {
+    Faq,
+    BeginnersGuide,
+    Audit
+  },
+  data() {
+    return {
+      activeTab: 'faq'
+    }
+  }
+}
+</script>
