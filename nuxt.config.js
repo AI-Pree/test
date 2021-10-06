19a92a2eb4288e2fb62a11ea4c361c91594bb55a
ai-pree <sexypistolcs@gmail.com>
2021-10-07 01:29:37 +1100

    changed port

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,34 +1,27 @@
 export default {
-
-  srcDir: './src/',
+  srcDir: "./src/",
   ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'la-front-temp',
+    title: "la-front-temp",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/scss/base.scss'
-  ],
+  css: ["@/scss/base.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '@plugins/web3.ts'
-  ],
+  plugins: ["@plugins/web3.ts"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -36,42 +29,39 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build",
     // lib amber
-    '@modules/amberlib',
+    "@modules/amberlib",
     // https://typed-vuex.roe.dev
-    'nuxt-typed-vuex'
+    "nuxt-typed-vuex"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/svg'
-  ],
+  modules: ["@nuxtjs/axios", "@nuxtjs/svg"],
 
   axios: {
     //baseUrl: process.env.baseUrl || 'http://34.64.168.231:1335/'
-    baseUrl: process.env.baseUrl || 'https://liquity-back.ambersoft.llc/'
+    baseUrl: process.env.baseUrl || "https://liquity-back.ambersoft.llc/"
   },
 
   // ENV
   env: {
     //baseUrl: process.env.BASE_URL || 'http://34.64.168.231:1335/',
-    baseUrl: process.env.BASE_URL || 'https://liquity-back.ambersoft.llc/',
-    mint: 'C6tfES3TrhTzQnRopAyqHAjx4ixShAzJ16QeffWvoXBk'
+    baseUrl: process.env.BASE_URL || "https://liquity-back.ambersoft.llc/",
+    mint: "C6tfES3TrhTzQnRopAyqHAjx4ixShAzJ16QeffWvoXBk"
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       config.node = {
-        fs: 'empty'
-      }
+        fs: "empty"
+      };
     }
   },
 
-  server:{
-    host: '0.0.0.0',
-    port: 3737
+  server: {
+    host: "0.0.0.0",
+    port: 5000
   }
-}
+};
