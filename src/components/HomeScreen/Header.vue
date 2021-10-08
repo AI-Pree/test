8b76528226466ecabeebae78d5da3d6536b2c8db
ai-pree <sexypistolcs@gmail.com>
2021-10-09 09:15:14 +1100

    added button and fixed alignment issue with the logo

--- src/components/HomeScreen/Header.vue
+++ src/components/HomeScreen/Header.vue
@@ -1,28 +1,36 @@
 <template>
   <div class="container pt-10-S pt-40-XS">
-    <div class="w-100 f-white-200 tt-u fs-14-S fs-25-XS fw-600 ta-c px-10-XS jc-c ai-c">
+    <div class="w-100 f-white-200 tt-u fs-14-S fs-25-XS fw-600 ta-c px-10-XS fd-r jc-c ai-c">
         <img src="@/assets/svg/company_logo.png" class="h-fix-15-S h-fix-55-XS mr-2" /> NEXFIN helps your FAMILY
     </div>
     <div class="w-100 gradient tt-u fs-13-S fs-30-XS fw-900 ta-c pt-4-S pt-15-XS">
       $ {{ getTotal }}
     </div>
     <div class="w-100 f-gray-400 ta-c fs-6-S fs-15-XS fw-500 pt-2-S pt-10-XS">
-      Total Deposited Value at HGEN
+        Total Deposited Value at GENS
+    </div>
+    <div class = "fd-r jc-c">
+        <AmButton color="mcolor-100" class = "mt-3" opacityEffect scaleEffect :full="mobile" @click="$emit('connect', 'connect')" v-if="!publicKey">
+            Launch App
+        </AmButton>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  computed: {
-    getTotal () {
-      let res = '000000000000'
-      const total = this.$accessor.totalDeposit
-      if (total) {
-        res = res.substr(0, res.length - total.toString().length) + total
-      }
-      return res.toString().replace(/(.)(?=(\d{3})+$)/g,'$1,')
+    props: {
+        publicKey: {type: String, default: null}
+    },
+    computed: {
+        getTotal () {
+        let res = '000000000000'
+        const total = this.$accessor.totalDeposit
+        if (total) {
+            res = res.substr(0, res.length - total.toString().length) + total
+        }
+        return res.toString().replace(/(.)(?=(\d{3})+$)/g,'$1,')
+        }
     }
-  }
 }
 </script>
